2a7c314451f71d11750ee24a6749a0642b124298
Prashanith <prashanith007@gmail.com>
2025-09-25 23:28:53 +0530

    routes

---
+++ lib/navigation/route_generator.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notion/navigation/routes.dart';
+
+class RouteGenerator {
+  final GlobalKey<NavigatorState> navigator = GlobalKey<NavigatorState>();
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.init:
+        return Page(const InitScreen());
+      case Routes.login:
+        return Page(const AuthenticationScreen());
+      case Routes.user:
+        return Page(const User());
+      default:
+        return Page(const AuthenticationScreen());
+    }
+  }
+}
+
+// ignore: non_constant_identifier_names
+Route<dynamic> Page(Widget widget) {
+  return MaterialPageRoute(builder: (_) => widget);
+}
